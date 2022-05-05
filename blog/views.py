3197a9ea2929243870f264c01dc6dec1acab58b7
null-po1nter <me.nullptr@gmail.com>
2022-05-05 19:18:25 +0300

    add post view and index template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(request, 'index.html', context={
+        'posts': posts,
+    })
