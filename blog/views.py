2c3d53e5b0176ee34ee812a73c10c7dfe53bd08b
null-po1nter <me.nullptr@gmail.com>
2022-05-05 19:28:15 +0300

    add post_detail view and template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -7,4 +7,12 @@ def post_list(request):
 
     return render(request, 'index.html', context={
         'posts': posts,
+    })
+
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, is_published=True)
+    
+    return render(request, 'post_detail.html', context={
+        'post': post
     })
