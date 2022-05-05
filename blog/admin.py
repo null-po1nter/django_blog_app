42ec2da47a6c8e287fc0c41034c68f6e881648a4
null-po1nter <me.nullptr@gmail.com>
2022-05-05 19:17:36 +0300

    register blog in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'slug', 'author', 'created_at', 'updated_at', 'published_at', 'is_published', 'tags')
+    search_fields = ('title', 'content', 'author')
+    list_filter = ('author', 'created_at', 'updated_at', 'published_at', 'is_published', 'tags')
+    list_display_links = ('title',)
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_at'
+    ordering = ('published_at',)
