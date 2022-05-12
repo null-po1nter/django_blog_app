ef0a25f8fe2f190d8c1363b0e6cdd8b455de3d86
null-po1nter <me.nullptr@gmail.com>
2022-05-12 21:19:28 +0300

    add auth

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.contrib.auth import urls
 from .views import SignUp
 
 
-app_name = 'users'
+# app_name = 'users'
 
 urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup')
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import logout
 from .forms import CreationForm
 
 
