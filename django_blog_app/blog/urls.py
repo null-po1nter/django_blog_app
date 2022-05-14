14b225068156711ba83aa393d313dd7e540bcab0
null-po1nter <me.nullptr@gmail.com>
2022-05-14 16:32:00 +0300

    add navigation by category

--- django_blog_app/blog/urls.py
+++ django_blog_app/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_list, add_post, post_detail, posts_by_tag
+from .views import post_list, add_post, post_detail, posts_by_category
 
 
 urlpatterns = [
     path('', post_list, name='index'),
     path('add_post/', add_post, name='add_post'),
     path('<slug:post>/', post_detail, name='post_detail'),
-    path('tag/<str:tag>/', posts_by_tag, name='post_list')
+    path('tag/<str:tag>/', post_list, name='post_list'),
+    path('category/<slug:category>', posts_by_category, name='posts_by_category'),
 ]
--- django_blog_app/blog/views.py
+++ django_blog_app/blog/views.py
@@ -7,9 +7,10 @@ from .models import Post
 from .forms import PostForm
 
 
-def post_list(request, tag=None):
+def post_list(request, tag=None, category=None):
     if tag:
         posts = Post.published.filter(tags__name__in=[tag])
+
     else:
         posts = Post.published.all()
 
@@ -66,4 +67,25 @@ def add_post(request):
     
     return render(request, 'post_edit.html', context={
         'form': form
+    })
+
+
+def posts_by_category(request, category):
+    posts = Post.published.filter(category__title=category)
+
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+
+    except PageNotAnInteger:            # if page is not an integer
+        posts = paginator.page(1)       # deliver the first page
+   
+    except EmptyPage:                                   # if page is out of range
+        posts = paginator.page(paginator.num_pages)     # deliver last page of results
+
+    return render(request, 'index.html', context={
+        'posts': posts,
+        page: 'pages',          # pass the page number
     })
